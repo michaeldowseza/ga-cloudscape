a7cb44fce5ff68aa6bd2d512f10cde3183804e73
Michael Dowse <dowsem@amazon.com>
2023-01-16 11:53:43 +0000

    Add tutorial start and end events

--- src/pages/wizard.tsx
+++ src/pages/wizard.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
 
 import Button from "@cloudscape-design/components/button";
 import Wizard, { WizardProps } from "@cloudscape-design/components/wizard";
@@ -7,7 +8,7 @@ const steps: WizardProps.Step[] = [
   {
     title: "Step 1",
     content: (
-      <div data-analytics-id="ga-step-1">
+      <div data-analytics-id="ga-tutorial-step-1">
         <div id="content-text">Content 1</div>
       </div>
     ),
@@ -15,7 +16,7 @@ const steps: WizardProps.Step[] = [
   {
     title: "Step 2",
     content: (
-      <div data-analytics-id="ga-step-2">
+      <div data-analytics-id="ga-tutorial-step-2">
         <div id="content-text">Content 2</div>
       </div>
     ),
@@ -23,7 +24,7 @@ const steps: WizardProps.Step[] = [
   {
     title: "Step 3",
     content: (
-      <div data-analytics-id="ga-step-3">
+      <div data-analytics-id="ga-tutorial-step-3">
         <div id="content-text">Content 3</div>
       </div>
     ),
@@ -47,12 +48,19 @@ const i18nStrings: WizardProps.I18nStrings = {
 function WizardPage() {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
 
+  useEffect(() => {
+    ReactGA.gtag("event", "tutorial_begin");
+  }, []);
+
   return (
     <Wizard
       steps={steps}
       i18nStrings={i18nStrings}
       activeStepIndex={activeStepIndex}
       onNavigate={(e) => setActiveStepIndex(e.detail.requestedStepIndex)}
+      onSubmit={() => {
+        ReactGA.gtag("event", "tutorial_complete");
+      }}
       secondaryActions={
         activeStepIndex === 2 ? <Button>Save as draft</Button> : null
       }
